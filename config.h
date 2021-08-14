cfa8d8819ed19297b308fb3eabccf30202583deb
Roland Nemes <nemesroland2@gmail.com>
2021-08-14 20:43:11 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* appearance */
 static char *font = "Terminus:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+
 static int borderpx = 2;
 
 /*
