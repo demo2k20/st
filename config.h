05bf74a19f5d6f928d1ef03c2071d1b266dc1b3a
demo2k20 <58609501+demo2k20@users.noreply.github.com>
2023-04-16 22:13:47 +0300

    Update config.h

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
-static char *font2[] = { "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
+static char *font2[] = { "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
 
 static int borderpx = 4;
 
